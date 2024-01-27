30bdea54e9a65a2adcfd16709d669b293b7550f6
tomiwadotJS <victorgbayedev@gmail.com>
2024-01-27 01:23:12 +0000

    fom card dropdown

---
+++ src/assets/wrappers/Dropdown.styles.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Dropdown = styled.div<{ isopen: boolean }>`
+    display: ${({ isopen }) => (isopen ? 'flex' : 'none')};
+    flex-direction: column;
+    position: absolute;
+    width: 190px;
+    height: auto;
+    padding: var(--XS, 8px);
+    align-items: flex-start;
+    border-radius: var(--XS, 8px);
+    border: 1px solid var(--cod-gray-border-divider, #E5E5E5);
+    background: var(--Neutral-Base, #FFF);
+    box-shadow: -2px 2px 12.1px 0px rgba(0, 0, 0, 0.05);
+
+    span{
+        display: flex;
+        padding: var(--XS, 8px);
+        align-items: center;
+        gap: var(--XS, 8px);
+        align-self: stretch;
+
+
+        p{
+            color: var(--Cod-Gray-Secondary-Text, #404040);
+            font-family: Quicksand;
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 22px; /* 157.143% */
+            letter-spacing: 0.5px;
+        }
+    }
+    span:hover{
+        border-radius: var(--XXS, 4px);
+        background: var(--Base-2-Highlight, #EDF1FF);
+    }
+`;
--- src/components/patterns/FormCard.tsx
+++ src/components/patterns/FormCard.tsx
@@ -1,7 +1,41 @@
 import { Wrapper } from "../../assets/wrappers/FormCard.styles"
 import ellipsis from '../../assets/images/ellipsis.svg'
+import { Dropdown } from "../../assets/wrappers/Dropdown.styles"
+import { useState, useEffect, useRef } from 'react';
+import eye from '../../assets/images/eye.svg'
+import chart from '../../assets/images/chart.svg'
+import clone from '../../assets/images/clone.svg'
+import file from '../../assets/images/file.svg'
+import share from '../../assets/images/share.svg'
+import trash from '../../assets/images/trash.svg'
+import users from '../../assets/images/users.svg'
 
 const FormCard = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null); 
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+        console.log('hey');
+    };
+
+    
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+        if (isOpen) {
+            // Attach event listener when the component mounts
+            document.addEventListener('mousedown', handleClickOutside);
+            
+        }
+        
+        // Detach event listener when the component unmounts
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isOpen]);
   return (
     <Wrapper>
         <div className="form-card-thumbnail">
@@ -12,7 +46,39 @@ const FormCard = () => {
                 <p className="form-card-title">Document Name</p>
                 <p className="form-card-description">Document long description</p>
             </div>
-            <img src={ellipsis}/>
+            <div>
+                <img src={ellipsis} onClick={toggleDropdown} style={{cursor:'pointer'}}/>
+                <Dropdown isopen={isOpen} ref={dropdownRef}>
+                    <span>
+                        <img src={eye}/>
+                        <p>Open</p>
+                    </span>
+                    <span>
+                        <img src={file}/>
+                        <p>Preview</p>
+                    </span>
+                    <span>
+                        <img src={share}/>
+                        <p>Share</p>
+                    </span>
+                    <span>
+                        <img src={chart}/>
+                        <p>Insight</p>
+                    </span>
+                    <span>
+                        <img src={users}/>
+                        <p>Recipients</p>
+                    </span>
+                    <span>
+                        <img src={clone}/>
+                        <p>Duplicate</p>
+                    </span>
+                    <span>
+                        <img src={trash}/>
+                        <p>Delete</p>
+                    </span>
+                </Dropdown>
+            </div>
         </div>
     </Wrapper>
   )
